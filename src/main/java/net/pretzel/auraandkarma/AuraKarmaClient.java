bec4446f316cea278ed9ff0d2841f12c4fcba1d2
MightyPretzelKing <bradyhottmann@gmail.com>
2025-01-02 14:26:29 -0800

    Initial commit

---
+++ src/main/java/net/pretzel/auraandkarma/AuraKarmaClient.java
@@ -0,0 +1,10 @@
+package net.pretzel.auraandkarma;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AuraKarmaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/pretzel/auraandkarma/AuraKarmaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pretzel.auraandkarma;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AuraKarmaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
